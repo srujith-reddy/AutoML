3fd6c834b2a77f06569f2578654681dbbe6f44b7
srujith <srujithreddy801@gmail.com>
2022-12-22 23:55:45 +0530

    web application created

--- Flaskapp.py
+++ Flaskapp.py
@@ -3,16 +3,17 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-@app.route("/",methods=['POST'])
-def hello_world():
+@app.route("/")
+def home():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET'])
 def upload():
-  file = request.files['file']
+  file = request.files['name']
   file.save('path/to/save/file.ext')
   return 'File uploaded successfully!'
   
 
 if __name__=="__main__":
     app.run(debug=True)
+    
