a3fb159be41ddc93444e0492fec64ce923114621
srujith <srujithreddy801@gmail.com>
2022-12-22 23:43:01 +0530

    web application template created

--- Flaskapp.py
+++ Flaskapp.py
@@ -3,18 +3,16 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['POST'])
 def hello_world():
-    return "Hello world"
+    return render_template('index.html')
 
-
-@app.route('/upload',methods=['POST'])
-def create_file():
-    file=request.files['file']
-    reader = csv.reader(file)
-    for row in reader:
-         print(row)
-
+@app.route('/upload', methods=['POST'])
+def upload():
+  file = request.files['file']
+  file.save('path/to/save/file.ext')
+  return 'File uploaded successfully!'
+  
 
 if __name__=="__main__":
     app.run(debug=True)
