695e91ca9ea6a2daa2990f022e2381ec6b210cd8
srujith <srujithreddy801@gmail.com>
2022-12-23 10:00:26 +0530

    Files are updated

--- Flaskapp.py
+++ Flaskapp.py
@@ -1,5 +1,14 @@
 import csv
+import os
+from werkzeug.utils import secure_filename
+from datetime import datetime
 from flask import Flask,render_template,request
+
+ALLOWED_EXTENSIONS=set(['csv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app=Flask(__name__)
 
@@ -7,10 +16,13 @@ app=Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/upload', methods=['GET'])
+@app.route('/upload', methods=['GET','POST'])
 def upload():
-  file = request.files['name']
-  file.save('path/to/save/file.ext')
+  file = request.files['uploaded_file']
+  if file and allowed_file(file.filename):
+        filename=secure_filename(file.filename)
+        new_filename= f'{filename.split(".")[0]}_{str(datetime.now())}.csv'
+        file.save(os.path.join('input',new_filename))
   return 'File uploaded successfully!'
   
 
